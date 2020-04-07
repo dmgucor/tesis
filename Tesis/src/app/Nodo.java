fee5b190111d756e6b12eab0ac63b3d931c89f7f
Diana Marcela <dm.gutierrez11@uniandes.edu.co>
2020-04-07 17:35:21 -0500

    delete maven usage

---
+++ Tesis/src/app/Nodo.java
@@ -0,0 +1,35 @@
+package app;
+
+public class Nodo<T> {
+	T info;
+	String name;
+	
+	public Nodo(String name) {
+		this.name = name;
+	}
+	
+	public void setInfo (T info) {
+		this.info = info;
+	}
+}
+
+
+//marco teórico: estado del arte los trabajos relacionados comentar las referencias, más descriptivo y extenso
+//Hablar de la librería a utilizar, las pruebas 
+//---------------------------------------------------------
+
+//1. Buscar conjunto de datos para probar (relaciones, objetos)    ckan.org  plataforma de datos abiertos
+//2. Visualizar datos (nodos, arcos -> nodo-nodo)
+
+//---------------------------------------------------
+//Poner el buscado de un color y los vecinos de otro
+//Mostrar secuencia: un formato en el cual se retorne una lista con los nodos consecutivos
+//Mostrar conjunto: solo reslatar los nodos
+
+//documento: incluir resultados preliminares: modulo de visualización se ha trabajdo con x herramienta, la definición d ela estructura d einput
+//se tiene un demo con patallazos para validar el funcionamiento d el aherramienta ccon las siguientes ufncionalidades
+//incluyendo la secuencia 
+//actualizar el estado del arte
+//cornogram actualizado
+
+//ir armando un demo
